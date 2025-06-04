4efc33b4960b474e79e20bb0fdd92e0cae4f07b1
ITBAtharizza <18223079@std.stei.itb.ac.id>
2025-06-04 23:16:59 +0700

    feat: DAOs

---
+++ app/src/main/java/model/entity/Doctor.java
@@ -0,0 +1,10 @@
+package model.entity;
+
+public class Doctor {
+    private String doctorId;
+    private int userId;
+    private String specialization;
+    private String licenseNumber;
+    private String availableDays;
+    private String availableHours;
+}
